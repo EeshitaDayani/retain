e299f2d1261737c32e16f14d12cdf660aaa86596
Eeshita Dayani <eeshitadayani.gmail.com>
2023-11-25 17:39:27 -0500

    Refacted AudioInput Component to only receive one audio recording at a time

--- src/components/AudioInput.js
+++ src/components/AudioInput.js
@@ -1,47 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { AudioRecorder } from 'react-audio-voice-recorder';
 
-const addAudioElement = (blob) => {
-  const url = URL.createObjectURL(blob);
-  console.log(url);
-  const audio = document.createElement("audio");
-  audio.src = url;
-  audio.controls = true;
-  document.body.appendChild(audio);
-
-  // Send the audio file to the server for text extraction
-  sendAudioToServer(blob);
-};
-
-const sendAudioToServer = async (audioBlob) => {
-  try {
-    const formData = new FormData();
-    formData.append('audio', audioBlob);
-
-    const response = await fetch('http://localhost:8080/api/extractTextFromAudio', {
-      method: 'POST',
-      body: formData,
-    });
-
-    const data = await response.json();
-    console.log('Text from audio:', data.text);
-  } catch (error) {
-    console.error('Error sending audio to server:', error);
-  }
-};
-
 export default function AudioInput() {
+  const [extractedText, setExtractedText] = useState('');
+  
+  const sendAudioToServer = async (audioBlob) => {
+    try {
+      const formData = new FormData();
+      formData.append('audio', audioBlob);
+  
+      const response = await fetch('http://localhost:8080/api/extractTextFromAudio', {
+        method: 'POST',
+        body: formData,
+      });
+  
+      const data = await response.json();
+      setExtractedText(data.text);
+    } catch (error) {
+      console.error('Error sending audio to server:', error);
+    }
+  };
+
   return (
     <div>
-      <h1>Audio recorder</h1>
+      <h2>Audio recorder</h2>
       <AudioRecorder 
-        onRecordingComplete={addAudioElement}
+        onRecordingComplete={sendAudioToServer}
         audioTrackConstraints={{
           noiseSuppression: true,
           echoCancellation: true,
         }} 
         downloadFileExtension="wav"
       />
+
+      {extractedText && (
+        <div>
+          <h2>Extracted Text:</h2>
+          <p>{extractedText}</p>
+        </div>
+      )}
     </div>
   );
 }
