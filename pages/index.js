54c5a8d44d40d4b240ebe128a1aa8dd25cf7a9c9
Eeshita Dayani <eeshitadayani.gmail.com>
2023-11-25 17:50:17 -0500

    Added toggle to switch between input types

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,44 @@
+import React, { useState } from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import InputField from '../src/components/InputField';
 import ImageInput from '../src/components/ImageInput';
 import AudioInput from '../src/components/AudioInput';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
+const ContentContainer = styled(Box)(({ theme }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(2),
 }));
 
 export default function Home() {
+  const [selectedInput, setSelectedInput] = useState('');
+
+  const handleChange = (event, newInput) => {
+    if (newInput !== null) {
+      setSelectedInput(newInput);
+    }
+  };
+
   return (
-      <div>
-        <h1>retain</h1>
-        <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={4}>
-            <Item>
-              <InputField />
-            </Item>
-          </Grid>
-          <Grid item xs={4}>
-            <Item>
-              <ImageInput />
-            </Item>
-          </Grid>
-          <Grid item xs={4}>
-            <Item>
-              <AudioInput />
-            </Item>
-          </Grid>
-        </Grid>
-      </Box>
-      </div>
-  )
+    <div>
+      <h1>retain</h1>
+      <ContentContainer>
+        <ToggleButtonGroup
+          value={selectedInput}
+          exclusive
+          onChange={handleChange}
+        >
+          <ToggleButton value="text">Text</ToggleButton>
+          <ToggleButton value="image">Image</ToggleButton>
+          <ToggleButton value="audio">Audio</ToggleButton>
+        </ToggleButtonGroup>
+
+        {selectedInput === 'text' && <InputField />}
+        {selectedInput === 'image' && <ImageInput />}
+        {selectedInput === 'audio' && <AudioInput />}
+      </ContentContainer>
+    </div>
+  );
 }
