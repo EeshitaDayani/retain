57cf9269dcadb75cf789d0aa0a1e9cca6a806ce2
Eeshita Dayani <eeshitadayani.gmail.com>
2023-11-24 21:50:29 -0500

    Added placeholder code for extracting text from images

--- pages/api/index.py
+++ pages/api/index.py
@@ -1,23 +1,26 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-# app instance
 app = Flask(__name__)
 CORS(app)
 
-# /api/home
 @app.route("/api/textInput", methods=['GET'])
-def return_home():
+def return_text_input():
     user_input = request.args.get('value', default='default_value')
-    # print("LOGGING", user_input)
-
-    # Create a response message
     response_message = "This is what you entered: " + user_input
 
     return jsonify({
         'message': response_message
     })
 
+@app.route("/api/extractText", methods=['POST'])
+def extract_text():
+    file = request.files['file']
+    print(file)
+
+    return jsonify({
+        'text': 'Received image'
+    })
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
--- src/components/ImageInput.js
+++ src/components/ImageInput.js
@@ -1,34 +1,68 @@
 import React, { useState } from 'react';
 
-const ImageInput = () => {
+export default function ImageInput() {
   const [selectedImage, setSelectedImage] = useState(null);
+  const [extractedText, setExtractedText] = useState('');
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    console.log(file);
+    setSelectedImage(file);
+  };
+
+  const removeImage = () => {
+    setSelectedImage(null);
+    setExtractedText('');
+  };
+
+  const extractTextFromImage = async () => {
+    try {
+      const formData = new FormData();
+      formData.append('file', selectedImage);
+  
+      const response = await fetch('http://localhost:8080/api/extractText', {
+        method: 'POST',
+        body: formData,
+      });
+  
+      const data = await response.json();
+      console.log(data.text);
+    } catch (error) {
+      console.error('Error extracting text:', error);
+    }
+  };
+  
+
   return (
     <div>
-      <h1>Upload and Display Image using React Hook's</h1>
+      <h1>Upload and Extract Text from Image</h1>
 
       {selectedImage && (
         <div>
           <img
-            alt="not found"
-            width={"250px"}
+            alt="Image Preview"
+            width={250}
             src={URL.createObjectURL(selectedImage)}
           />
-          <br />
-          <button onClick={() => setSelectedImage(null)}>Remove</button>
+
+          <button onClick={removeImage}>Remove</button>
         </div>
       )}
-      <br />
-      <br />
+
       <input
         type="file"
         name="myImage"
-        onChange={(event) => {
-          console.log(event.target.files[0]);
-          setSelectedImage(event.target.files[0]);
-        }}
+        onChange={handleImageChange}
       />
+
+      <button onClick={extractTextFromImage}>Extract Text</button>
+
+      {extractedText && (
+        <div>
+          <h2>Extracted Text:</h2>
+          <p>{extractedText}</p>
+        </div>
+      )}
     </div>
   );
-};
-
-export default ImageInput;
+}
